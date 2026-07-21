cb428758e7a45ce3f0c814e94f36a9859c80b5dc
haxscramper <haxscramper@gmail.com>
2026-07-21 20:45:17 +0400

    checkpoint

--- tests/e2e/test_file_tree_build.py
+++ tests/e2e/test_file_tree_build.py
@@ -1,8 +1,20 @@
 from pathlib import Path
 
-from tests.utils import init_index_service
+from tests.utils import init_index_service, init_file_tree_config
 
 
 def test_e2e_tree_build(stable_test_dir: Path):
-    service = init_index_service(stable_test_dir)
-    service.service.run_index()
+    index = init_index_service(stable_test_dir)
+
+    dir = index.root_dir
+    dir.joinpath("a.txt").write_text("random_content")
+    dir.joinpath("b.txt").write_text("whatever")
+
+    sub = dir / "sub"
+    sub.mkdir(exist_ok=True, parents=True)
+    sub.joinpath("sub_a.txt").write_text("nested")
+    sub.joinpath("sub_b.txt").write_text("whatever 1123")
+
+    index.service.run_index()
+
+    tree_config = init_file_tree_config(index)
